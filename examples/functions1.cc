e22afdce94e5b42a77ae83ab9b53caa5bd65229c
Timo Bingmann <tbgit@panthema.net>
2010-07-18 19:36:15 +0000

    Adding copyright notices to all source files.

--- examples/functions1.cc
+++ examples/functions1.cc
@@ -2,6 +2,25 @@
 // $Id$
 
 /*
+ * STX Execution Pipe Library v0.7.0
+ * Copyright (C) 2010 Timo Bingmann
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+/*
  * This example shows how to use the function classes stx::PipeSource and
  * stx::PipeFunction to insert custom processing into a pipe sequence. The
  * application calls tar to create an archive, calculates the SHA1 digest of
--- src/stx-execpipe.h
+++ src/stx-execpipe.h
@@ -1,4 +1,24 @@
 // -*- mode: c++; fill-column: 79 -*-
+// $Id$
+
+/*
+ * STX Execution Pipe Library v0.7.0
+ * Copyright (C) 2010 Timo Bingmann
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
 
 #ifndef _STX_EXECPIPE_H_
 #define _STX_EXECPIPE_H_
@@ -98,21 +118,12 @@ public:
 };
 
 /**
- * df
+ * \brief Main library interface (reference counted pointer)
  *
- *
- *
- * <pre>
- * 
- *  Input Stream                Pipe Stages                      Output Stream
- * 
- *      none    |                                                |    none
- *       fd     |                  exec()                        |     fd
- *      file    |--> stage -->       or     --> stage --> ... -->|    file
- *     string   |              PipeFunction                      |   string
- *   PipeSource |                                                |  PipeSink
- * 
- * </pre>
+ * The ExecPipe class is the main interface to the library. It is a reference
+ * counted pointer implementation, so you can easily copy and pass around
+ * without duplicating the inside object. See the \ref index "main page" for
+ * detailed information and examples.
  */
 class ExecPipe
 {
